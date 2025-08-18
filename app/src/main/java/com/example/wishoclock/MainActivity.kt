8ea36b8a6c266d52c80e972c759c9248423a08a6
chaitanya harish <chaitanyaharish080@gmail.com>
2025-08-18 18:06:15 +0530

    Main Activity and font Added

--- app/src/main/java/com/example/wishoclock/MainActivity.kt
+++ app/src/main/java/com/example/wishoclock/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.wishoclock
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.wishoclock.R.id.add_wish_btn
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val topBar_Title = findViewById<TextView>(R.id.topbar_Text)
+        val subtitle = findViewById<TextView>(R.id.subtitle)
+        val add_wish_btn : Button = findViewById<Button>(add_wish_btn)
 
     }
 }
